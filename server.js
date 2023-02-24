8fd4862cb3abfeacc79344106119aca3fd9da310
allisonnault <allisonrnault@gmail.com>
2023-02-24 17:03:21 -0600

    notes updating to page

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const uuid = require('./helpers/uuid');
 const express = require('express');
 const path = require('path');
+const dataList = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,5 +19,26 @@ res.sendFile(path.join(__dirname, '/public/index.html'))
 app.get('/notes', (req, res) => 
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res)=> {
+    res.json(dataList)
+});
+
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    const newNote = {
+        title,
+        text,
+        id: uuid(),
+    };
+    readAndAppend(newNote, './db/db.json');
+    const response = {
+        status: "success",
+        body: newNote,
+    };
+    res.json(response);
+
+})
+
 
 app.listen(PORT, ()=> console.log(`listening on PORT: ${PORT}`));
